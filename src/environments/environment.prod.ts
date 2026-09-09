c876bdd61711d9bd5e50968c5d88b1a21f649b4d
juan vargas <juanpi_vl@outlook.com>
2026-09-09 10:41:02 -0700

    cambio de variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://jp.tail5cbc3e.ts.net:8443/api/v1/',
+  apiUrl: 'https://api.serviciosmedicosrise.com/api/v1/',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001/api/v1/',
+  apiUrl: 'https://api.serviciosmedicosrise.com/api/v1/',
 };
